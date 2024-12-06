833a87b9e1451fb5ae535c7cb3a763885f94b7b8
Christopher <christopher_oo1@yahoo.com.sg>
2024-12-06 21:10:59 +0800

    done with day 6

--- 06/main.py
+++ 06/main.py
@@ -1,19 +1,24 @@
+from time import sleep
+from copy import deepcopy
+
+
 def traverse(lab_map, pos):
     dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     marks = ["|", "-"]
     diri = 0
     distinct_positions = 0
-    lab_map[pos[0]][pos[1] - 1] = "#"
+    temp_map = [[0 for _ in range(len(lab_map[0]))] for _ in range(len(lab_map))]
     while True:
         inc_x, inc_y = dirs[diri]
         if lab_map[pos[0]][pos[1]] not in "|-+":
             distinct_positions += 1
         mark = diri % 2
-        if lab_map[pos[0]][pos[1]] == ".":
+        if lab_map[pos[0]][pos[1]] == "." or lab_map[pos[0]][pos[1]] == "^":
             lab_map[pos[0]][pos[1]] = marks[mark]
         elif lab_map[pos[0]][pos[1]] != marks[mark]:
             lab_map[pos[0]][pos[1]] = "+"
-        elif lab_map[pos[0]][pos[1]] in "|-+":
+        temp_map[pos[0]][pos[1]] += 1
+        if temp_map[pos[0]][pos[1]] > 5:
             return 0, lab_map
         temp_pos = (pos[0] + inc_x, pos[1] + inc_y)
         if not (0 <= temp_pos[0] < len(lab_map) and 0 <= temp_pos[1] < len(lab_map[0])):
@@ -26,34 +31,30 @@ def traverse(lab_map, pos):
 
 def obstruct(new_map, lab_map, pos):
     obstacles = 0
-    crosses = [
-        (x, y)
-        for x in range(len(new_map))
-        for y in range(len(new_map[0]))
-        if new_map[x][y] == "+"
-    ]
-    print(crosses)
-    potential = []
-    for i in range(len(crosses)):
-        for j in range(i + 1, len(crosses)):
-            intersects = []
-            x1, y1 = crosses[i]
-            x2, y2 = crosses[j]
-            if x1 == x2 or y1 == y2:
+    for x in range(len(new_map)):
+        for y in range(len(new_map[0])):
+            temp_map = deepcopy(lab_map)
+            check = []
+            if 0 < x:
+                check.append(new_map[x - 1][y])
+            if x < len(new_map) - 1:
+                check.append(new_map[x + 1][y])
+            if 0 < y:
+                check.append(new_map[x][y - 1])
+            if y < len(new_map[0]) - 1:
+                check.append(new_map[x][y + 1])
+            if not any(c in "|-+" for c in check):
                 continue
-            intersects.append((x1, y2))
-            intersects.append((x2, y1))
-            hors = [new_map[x][min(y1, y2) + 1 : max(y1, y2)] for x in (x1, x2)]
-            vers = [
-                [new_map[x][y] for x in range(min(x1, x2) + 1, max(x1, x2))]
-                for y in (y1, y2)
-            ]
-            print(x1, y1, x2, y2)
-            print("hors ", hors, "vers ", vers)
+            if temp_map[x][y] == ".":
+                temp_map[x][y] = "#"
+                distinct, _ = traverse(temp_map, pos)
+                if distinct == 0:
+                    obstacles += 1
+    return obstacles
 
 
 def main():
-    file = "input.txt"
+    file = "inputs.txt"
     lab_map = []
     position = (0, 0)
     with open(file, "r") as f:
@@ -62,11 +63,11 @@ def main():
             if "^" in line:
                 position = (i, line.index("^"))
             lab_map.append(list(line.strip()))
-    distinct, new_map = traverse(lab_map, position)
-    obstruct(new_map, lab_map, position)
-    for row in lab_map:
-        print("".join(row))
+    temp_map = deepcopy(lab_map)
+    distinct, new_map = traverse(temp_map, position)
     print("Distinct: ", distinct)
+    obstacles = obstruct(new_map, lab_map, position)
+    print("Obstacles: ", obstacles)
 
 
 if __name__ == "__main__":
